cf0bc02768aeb98caa13e7f114cdf28dd557c21f
ErickBrayan <ebrayan2000@gmail.com>
2023-09-17 22:05:19 -0500

    testbranch

--- src/main/java/com/example/demorestcibertec/entities/AutorEntity.java
+++ src/main/java/com/example/demorestcibertec/entities/AutorEntity.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "autor")
 public class AutorEntity extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
     @Column(name = "lastname")
     private String lastName;
