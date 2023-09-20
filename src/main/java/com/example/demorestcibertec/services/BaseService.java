61eb036bf64b58bf89349ef7131a3ed37103b55a
ErickBrayan <ebrayan2000@gmail.com>
2023-09-19 21:06:18 -0500

    delete line

--- src/main/java/com/example/demorestcibertec/services/BaseService.java
+++ src/main/java/com/example/demorestcibertec/services/BaseService.java
@@ -9,7 +9,6 @@ public interface BaseService<E extends BaseEntity,ID extends Serializable> {
 
      List<E> findAll() throws Exception;
      public E findById(ID id) throws Exception;
-
      E save(E e) throws Exception;
      E update(ID id, E e) throws Exception;
      boolean delete(ID id) throws Exception;
